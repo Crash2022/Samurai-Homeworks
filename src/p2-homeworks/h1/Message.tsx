8a6bc0b2348f547740b0fbcdd3488a03b753b75a
Crash <crash555@mail.ru>
2022-08-18 11:52:12 +0500

    hw1 done

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,11 +1,34 @@
 import React from 'react'
+import classes from './Message.module.css'
 
-function Message() {
+type MessagePropsType = {
+    avatar: string,
+    message: string,
+    name: string,
+    time: string
+}
+
+function Message(props: MessagePropsType) {
     return (
-        <div>
-
+        <div className={classes.message}>
+            <div className={classes.avatar}>
+                <img src={props.avatar}></img>
+            </div>
+            <div className={classes.info}>
+                <div className={classes.name}>
+                    {props.name}
+                </div>
+                <div className={classes.text_message}>
+                    <div className={classes.text}>
+                        {props.message}
+                    </div>
+                    <div className={classes.time}>
+                        {props.time}
+                    </div>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
 
 export default Message
