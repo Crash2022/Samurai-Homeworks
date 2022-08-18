d0d5e69ae995b44914f5b0f9bfbacd99829c8d7a
Crash <crash555@mail.ru>
2022-08-18 12:40:32 +0500

    alternative message done

--- src/p2-homeworks/h1/AlternativeMessage.tsx
+++ src/p2-homeworks/h1/AlternativeMessage.tsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import classes from './AlternativeMessage.module.css'
 
-function AlternativeMessage() {
+type MessagePropsType = {
+    avatar: string,
+    message: string,
+    name: string,
+    time: string
+}
+
+function AlternativeMessage(props: MessagePropsType) {
     return (
-        <div>
-
+        <div className={classes.message}>
+            <div className={classes.avatar}>
+                <img src={props.avatar}></img>
+            </div>
+            <div className={classes.info}>
+                <div className={classes.name}>
+                    {props.name}
+                </div>
+                <div className={classes.text_message}>
+                    <div className={classes.text}>
+                        {props.message}
+                    </div>
+                    <div className={classes.time}>
+                        {props.time}
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
