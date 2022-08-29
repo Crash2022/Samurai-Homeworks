e61d01e3806c80f9df488ad0725cd30d5da006b4
Crash2022 <crash555@mail.ru>
2022-08-29 10:11:11 +0500

    add classes color

--- src/p2-homeworks/h2/Affair.tsx
+++ src/p2-homeworks/h2/Affair.tsx
@@ -12,6 +12,8 @@ type AffairPropsType = {
 function Affair(props: AffairPropsType) {
     const deleteCallback = () => {props.deleteAffairCallback(props.id)}// need to fix
 
+    const priorityClass = classes[props.affair.priority]
+
     return (
         <>
             {/*<div className={classes.affairs_items}>
@@ -29,7 +31,7 @@ function Affair(props: AffairPropsType) {
             <tbody>
             <tr>
                 <td style={{paddingRight: "20px"}}>{props.affair.name}</td>
-                <td style={{paddingRight: "10px"}}>{props.affair.priority}</td>
+                <td style={{paddingRight: "10px"}} className={priorityClass}>{props.affair.priority}</td>
                 <td><button onClick={deleteCallback} className={classes.affairs_items_buttonStyle}>X</button></td>
             </tr>
             </tbody>
