0fcbd6c93a75830ba95db48b7ed0d8773182c5b1
Crash2022 <crash555@mail.ru>
2022-09-14 11:36:46 +0500

    add menu v2

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -4,6 +4,7 @@ import style from './Header.module.css'
 
 function Header() {
     return (
+        <>
         <div className={style.header}>
             <div className={style.headerMenu}>
                 Меню
@@ -16,6 +17,14 @@ function Header() {
                 <NavLink to={'/senior'}>Senior</NavLink>
             </div>
         </div>
+        <div className={style.sliderMenu}>
+            <NavLink to={'/pre-junior'}>PreJunior</NavLink>
+            <NavLink to={'/junior'}>Junior</NavLink>
+            <NavLink to={'/middle'}>Middle</NavLink>
+            <NavLink to={'/senior'}>Senior</NavLink>
+            <span>Меню</span>
+        </div>
+        </>
     )
 }
 
