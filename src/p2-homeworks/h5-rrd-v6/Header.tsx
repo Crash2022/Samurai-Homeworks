0254a30f6843157509be9201d6c1c983da58cb42
Crash2022 <crash555@mail.ru>
2022-09-14 14:18:51 +0500

    add nav color

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {NavLink} from "react-router-dom";
 import style from './Header.module.css'
+//import '../../p1-main/m1-ui/u1-app/App.css'
 
 function Header() {
     return (
@@ -11,17 +12,43 @@ function Header() {
             </div>
             <div className={style.headerLinks}>
                 {/*// add NavLinks*/}
-                <NavLink to={'/pre-junior'}>PreJunior</NavLink>
-                <NavLink to={'/junior'}>Junior</NavLink>
-                <NavLink to={'/middle'}>Middle</NavLink>
-                <NavLink to={'/senior'}>Senior</NavLink>
+                <NavLink to={'/pre-junior'}
+                         /*style={(params) => {
+                             return {color: params.isActive ? 'orange' : 'black'}
+                         }}*/
+                         className={ ({isActive}) => isActive ? style.act : style.def }
+                >
+                    PreJunior
+                </NavLink>
+                <NavLink to={'/junior'}
+                         className={ ({isActive}) => isActive ? style.act : style.def }
+                >
+                    Junior
+                </NavLink>
+                <NavLink to={'/middle'}
+                         className={ ({isActive}) => isActive ? style.act : style.def }
+                >
+                    Middle
+                </NavLink>
+                <NavLink to={'/senior'}
+                         className={ ({isActive}) => isActive ? style.act : style.def }
+                >
+                    Senior
+                </NavLink>
             </div>
         </div>
         <div className={style.sliderMenu}>
-            <NavLink to={'/pre-junior'}>PreJunior</NavLink>
-            <NavLink to={'/junior'}>Junior</NavLink>
-            <NavLink to={'/middle'}>Middle</NavLink>
-            <NavLink to={'/senior'}>Senior</NavLink>
+            <NavLink to={'/pre-junior'}
+                /*style={(params) => {
+                    return {color: params.isActive ? 'orange' : ''}
+                }}*/
+                     className={ ({isActive}) => isActive ? style.act2 : style.def2 }
+            >
+                PreJunior
+            </NavLink>
+            <NavLink to={'/junior'} className={ ({isActive}) => isActive ? style.act2 : style.def2 }>Junior</NavLink>
+            <NavLink to={'/middle'} className={ ({isActive}) => isActive ? style.act2 : style.def2 }>Middle</NavLink>
+            <NavLink to={'/senior'} className={ ({isActive}) => isActive ? style.act2 : style.def2 }>Senior</NavLink>
             <span>Меню</span>
         </div>
         </>
