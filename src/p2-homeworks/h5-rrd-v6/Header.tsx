3cb02c59490a759e7d161051ce68cb3a2fa0a5f1
Crash2022 <crash555@mail.ru>
2022-09-14 11:07:29 +0500

    refactoring styles

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -10,10 +10,10 @@ function Header() {
             </div>
             <div className={style.headerLinks}>
                 {/*// add NavLinks*/}
-                <NavLink to={'/pre-junior'} style={{paddingRight: '20px'}}>PreJunior</NavLink>
-                <NavLink to={'/junior'} style={{paddingRight: '20px'}}>Junior</NavLink>
-                <NavLink to={'/middle'} style={{paddingRight: '20px'}}>Middle</NavLink>
-                <NavLink to={'/senior'} style={{paddingRight: '20px'}}>Senior</NavLink>
+                <NavLink to={'/pre-junior'}>PreJunior</NavLink>
+                <NavLink to={'/junior'}>Junior</NavLink>
+                <NavLink to={'/middle'}>Middle</NavLink>
+                <NavLink to={'/senior'}>Senior</NavLink>
             </div>
         </div>
     )
