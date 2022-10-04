b1c5b016bf3255d07afdb4edd070ecc70909d46e
Crash2022 <crash555@mail.ru>
2022-10-04 16:36:02 +0500

    sort up & down done

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,47 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from "../HW8";
+
+export const homeWorkReducer = (state: Array<UserType>, action: ActionsType): Array<UserType> => { // need to fix any
     switch (action.type) {
-        case 'sort': {
-            // need to fix
-            return state
+        case SORT_UP: {
+            console.log('SORT_UP');
+            return [...state.sort((a,b) => {
+                    return a.name.localeCompare(b.name);
+                }
+            )];
+        }
+        case SORT_DOWN: {
+            console.log('SORT_DOWN');
+            return [...state.sort((a,b) => {
+                    return b.name.localeCompare(a.name);
+                }
+            )];
         }
         case 'check': {
             // need to fix
             return state
         }
         default: return state
+    }
+}
+
+export type ActionsType = UpSortACType | DownSortACType
+
+const SORT_UP = 'SORT_UP';
+export type UpSortACType = ReturnType<typeof UpSortAC>
+
+export const UpSortAC = (payload: string) => {
+    return {
+        type: SORT_UP,
+        payload: payload
+    }
+}
+
+const SORT_DOWN = 'SORT_DOWN';
+export type DownSortACType = ReturnType<typeof DownSortAC>
+
+export const DownSortAC = (payload: string) => {
+    return {
+        type: SORT_DOWN,
+        payload: payload
     }
 }
