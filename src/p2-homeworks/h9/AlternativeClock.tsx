73dae65267453b2745f849036dbf15e0092e9342
Crash2022 <crash555@mail.ru>
2022-10-11 22:17:34 +0500

    hw9 moment lib add

--- src/p2-homeworks/h9/AlternativeClock.tsx
+++ src/p2-homeworks/h9/AlternativeClock.tsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
+import moment, {MomentInput} from "moment";
 
 function AlternativeClock() {
 
+    const [date, setDate] = useState<MomentInput>(moment());
+
+    setInterval(()=> {
+        setDate(moment());
+    }, 1000)
+
+    const currentTime = moment().format('h:mm:ss');
 
     return (
-        <div>
-
+        <div style={{marginLeft: '10px'}}>
+            <h4>Moment Library Test</h4>
+            <div>
+                {moment().format('DD.MM.YYYY')}
+            </div>
+            <div>
+                {currentTime}
+            </div>
         </div>
     )
 }
 
-export default AlternativeClock
+export default AlternativeClock;
