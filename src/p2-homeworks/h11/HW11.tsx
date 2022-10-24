9281cf6407701b0675af94c50e7995f33c2e6281
Crash2022 <crash555@mail.ru>
2022-10-25 00:01:15 +0500

    hw11 main done

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -3,8 +3,8 @@ import SuperRange from './common/c7-SuperRange/SuperRange'
 import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
 
 function HW11() {
-    const [value1, setValue1] = useState(0)
-    const [value2, setValue2] = useState(100)
+    const [value1, setValue1] = useState<number>(0)
+    const [value2, setValue2] = useState<number>(100)
 
     const onChangeRangeValue = (value: number) => {
         setValue1(value);
@@ -19,6 +19,7 @@ function HW11() {
             <div>
                 <span style={{display: 'inline-block', width:'30px'}}>{value1}</span>
                 <SuperRange
+                    value={value1}
                     onChangeRange={onChangeRangeValue}
                 />
             </div>
@@ -27,6 +28,8 @@ function HW11() {
                 <span style={{display: 'inline-block', width:'30px'}}>{value1}</span>
                 <SuperDoubleRange
                     // сделать так чтоб value1 и value2 изменялось
+                    value={value1}
+                    onChangeRange={onChangeRangeValue}
                 />
                 <span style={{display: 'inline-block', width:'20px', marginLeft: '5px'}}>{value2}</span>
             </div>
--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,22 +1,52 @@
-import React from 'react'
+import React, {ChangeEvent, DetailedHTMLProps, InputHTMLAttributes} from 'react'
+import s from "../c7-SuperRange/SuperRange.module.css";
 
-type SuperDoubleRangePropsType = {
-    onChangeRange?: (value: [number, number]) => void
-    value?: [number, number]
+// тип пропсов обычного инпута
+type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+
+type SuperDoubleRangePropsType = DefaultInputPropsType & {
+    onChangeRange?: (value: number) => void
+    //onChangeRange?: (value: [number, number]) => void
+
+    //value?: [number, number]
     // min, max, step, disable, ...
+
+    // min: number
+    // max: number
+    // step: number
+    // disable: boolean
 }
 
 const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
     {
-        onChangeRange, value,
+        onChange, onChangeRange,
+        //value,
         // min, max, step, disable, ...
+        //min, max, step, disable,
+        className,
+
+        ...restProps// все остальные пропсы попадут в объект restProps
     }
 ) => {
+
     // сделать самому, можно подключать библиотеки
+
+    const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
+        onChange && onChange(e) // сохраняем старую функциональность
+        onChangeRange && onChangeRange(+e.currentTarget.value)
+    }
+
+    const finalRangeClassName = `${s.range} ${className ? className : ''}`
 
     return (
         <>
-            DoubleRange
+            <input
+                type={'range'}
+                onChange={onChangeCallback}
+                className={finalRangeClassName}
+
+                {...restProps}
+            />
         </>
     )
 }
