ad6149af740484b3e4952553883fc2c140c2a307
Crash2022 <crash555@mail.ru>
2022-11-05 14:35:54 +0500

    hw13 started

---
+++ src/p2-homeworks/h13/HW13.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import s from "../h13/HW13.module.css";
+
+function HW13() {
+
+    return (
+        <div className={s.default}>
+            <hr/>
+            <div>hw13</div>
+            <hr/>
+        </div>
+    );
+}
+
+export default HW13;
