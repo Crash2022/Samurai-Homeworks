214694af6392ca43b647e9875533ef6f7ed84100
Crash2022 <crash555@mail.ru>
2023-03-09 12:49:08 +0500

    fix: hw14 import

--- src/p2-homeworks/h5-rrd-v6/pages/Senior.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/Senior.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import HW13 from "../../h13/HW13";
+import HW14 from "../../hw14/HW14";
+import HW15 from "../../hw15/HW15";
 
 function Senior() {
     return (
         <div style={{margin: '20px'}}>
             Senior. Ты уже бородартый дядька, но отдыхать некогда :)
             <HW13/>
+            <HW14/>
+            {/*<HW15/>*/}
         </div>
     )
 }
