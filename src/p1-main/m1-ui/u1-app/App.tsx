157b9f22cac55f8eb690e1b5ca091403e122a264
Crash2022 <crash555@mail.ru>
2023-03-09 12:58:29 +0500

    refactor: App imports

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -1,23 +1,10 @@
 import React from 'react'
 import s from './App.module.css'
-
 import HW1 from '../../../p2-homeworks/h1/HW1'
 import HW2 from '../../../p2-homeworks/h2/HW2'
 import HW3 from '../../../p2-homeworks/h3/HW3'
 import HW4 from '../../../p2-homeworks/h4/HW4'
-
 import HW5 from "../../../p2-homeworks/h5-rrd-v6/HW5"
-import HW6 from "../../../p2-homeworks/h6/HW6"
-import HW7 from "../../../p2-homeworks/h7/HW7"
-import HW8 from "../../../p2-homeworks/h8/HW8"
-import HW9 from "../../../p2-homeworks/h9/HW9"
-import HW10 from "../../../p2-homeworks/h10/HW10"
-import HW11 from "../../../p2-homeworks/h11/HW11"
-import HW12 from "../../../p2-homeworks/h12/HW12"
-import HW13 from "../../../p2-homeworks/h13/HW13"
-
-import HW14 from "../../../p2-homeworks/hw14/HW14"
-import HW15 from "../../../p2-homeworks/hw15/HW15"
 
 function App() {
     return (
@@ -29,18 +16,6 @@ function App() {
             {/*<HW4/>*/}
 
             <HW5/>
-
-            {/*<HW6/>*/}
-            {/*<HW7/>*/}
-            {/*<HW8/>*/}
-            {/*<HW9/>*/}
-            {/*<HW10/>*/}
-            {/*<HW11/>*/}
-            {/*<HW12/>*/}
-            {/*<HW13/>*/}
-
-            {/*<HW14/>*/}
-            {/*<HW15/>*/}
         </div>
     )
 }
